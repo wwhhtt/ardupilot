2b0c552c3edaa8c4dbb02b3d672762316d444ebc
priseborough <p_riseborough@live.com.au>
2016-07-05 08:12:20 +1000

    ONLY FOR SOLO - Reinstate EKF1
    This was removed upstream because of the bug in the STM-32 micro revision used by most pixhawks that meant the upper 1MB of flash could not be used with high rate USB comms.
    The PX2 in Solo uses a later revision without this limitation.

--- libraries/AP_AHRS/AP_AHRS_NavEKF.h
+++ libraries/AP_AHRS/AP_AHRS_NavEKF.h
@@ -43,7 +43,8 @@
   space
  */
 #if CONFIG_HAL_BOARD == HAL_BOARD_PX4 && (defined(CONFIG_ARCH_BOARD_PX4FMU_V1) || defined(CONFIG_ARCH_BOARD_PX4FMU_V2))
-#define AP_AHRS_WITH_EKF1 0
+// Solo can use additional flash space because of the STM32 chip revision in the PX2
+#define AP_AHRS_WITH_EKF1 1
 #else
 #define AP_AHRS_WITH_EKF1 1
 #endif
